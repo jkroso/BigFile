c3f64c29db49d0c824f84010ee7bc6c482e12ffc
jkroso <jkroso@gmail.com>
2013-04-09 10:45:15 +1200

    update sourcegraph for development build

--- src/middleware/umd.js
+++ src/middleware/umd.js
@@ -1,6 +1,6 @@
 /**
  * Wrap the code in a univeral module definer (UMD)
- * This will protect the code from creating globals also since everything ends 
+ * This will protect the code from creating globals since everything ends 
  * up being wrapped in function. UMD covers commonjs, AMD, and falls back to
  * global exports
  *
@@ -16,9 +16,9 @@ module.exports = function (code, next) {
 function umd (name, entryPath, code) {
 	return [
 	    "!function (context, definition) {"
-	  , "	if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {"
+	  , "	if (typeof require == 'function' && typeof exports == 'object' && typeof module == 'object') {"
 	  , "		module.exports = definition()"
-	  , "	} else if (typeof define === 'function' && typeof define.amd  === 'object') {"
+	  , "	} else if (typeof define == 'function' && typeof define.amd  == 'object') {"
 	  , "		define(definition)"
 	  , "	} else {"
 	  , "		" + (name == 'null' || name == null ? "" : "context['"+name+"'] = ") + "definition()"
